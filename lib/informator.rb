ffd877d01f9b0f3ff774e94ac2c3ee7b9909fdf8
Andrew Kozin <andrew.kozin@gmail.com>
2015-05-23 22:38:11 +0300

    [FEATURE] Informator API
    API methods are added to Informator module.
    
    == Addons
    
    * Informator#subscribe
    * Informator#remember
    * Informator#publish
    * Informator#publish!

--- lib/informator.rb
+++ lib/informator.rb
@@ -8,4 +8,75 @@ require_relative "informator/reporter"
 #
 module Informator
 
+  # Subscribes the listener for receiving events notifications via callback
+  #
+  # @param [Object] listener
+  #   The object to send events to
+  # @param [#to_sym] callback (:receive)
+  #   The name of the listener method to send events through
+  #
+  # @return [self] itself
+  #
+  def subscribe(listener, callback = :receive)
+    sub = Subscriber.new(listener, callback)
+    __subscribers__ << sub unless __subscribers__.include? sub
+
+    self
+  end
+
+  # @!method remember(type, messages, data)
+  # Builds and stores the event waiting for being published
+  #
+  # @param (see Informator::Event.new)
+  #
+  # @return [self] itself
+  #
+  def remember(*args)
+    __reporter__.remember(*args)
+
+    self
+  end
+
+  # @overload publish(type, messages, data)
+  #   Builds the event and then publishes all unpublished events
+  #
+  #   @param (see #remember)
+  #
+  # @overload publish
+  #   Publishes all unpublished events without adding a new one
+  #
+  # @return [Array<Informator::Event>] list of events having been published
+  #
+  def publish(*args)
+    remember(*args) if args.any?
+    __reporter__.notify __subscribers__
+  end
+
+  # The same as `publish` except for it throws `:published` afterwards
+  #
+  # @overload publish(type, messages, data)
+  #   Builds the event and then publishes all unpublished events
+  #
+  #   @param (see #remember)
+  #
+  # @overload publish
+  #   Publishes all unpublished events without adding a new one
+  #
+  # @raise [UncaughtThrowError, ArgumentError]
+  #   for `:published` events being carried
+  #
+  def publish!(*args)
+    throw :published, publish(*args)
+  end
+
+  private
+
+  def __reporter__
+    @__reporter__ ||= Reporter.new
+  end
+
+  def __subscribers__
+    @__subscribers__ ||= []
+  end
+
 end # module Informator
